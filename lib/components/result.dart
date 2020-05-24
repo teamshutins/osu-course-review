b4918d09fb43bb0f9aa632649c5f53ba5ead9004
David Chen <zdent09@gmail.com>
2020-05-24 11:36:49 +0800

    remove old commented out code

--- lib/components/result.dart
+++ lib/components/result.dart
@@ -29,10 +29,6 @@ class Result extends StatelessWidget {
           instructor: course['instructor']['fullName'],
           fontSize: 20
         ).buildResultText(),
-        // child: Text(
-        //   '${course['title']}_${course['section']} ${course['instructor']['fullName']}',
-        //   style: TextStyle(fontSize: 20)
-        // ),
       ),
     );
   }
