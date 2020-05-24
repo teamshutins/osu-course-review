21b7ca6532e2d4117083253b2ba88a71ebf9046a
Taka Watanabe <38120823+watantak@users.noreply.github.com>
2020-05-24 16:37:12 -0700

    Revert "Add comments 1"

--- lib/builder/result_text_builder.dart
+++ lib/builder/result_text_builder.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-// This is antoher implementation of the "builder" design pattern with a different approach.
-// In this implementation, the constructor takes care of building products with different combinations 
-// of four parameters.
-// Then, the buildResultText() method returns a text object to display the course search results.
 class ResultTextBuilder {
   final String title;
   final int section;
