456d14851693a80d46ea18e207b61400fe02e076
David Chen <zdent09@gmail.com>
2020-06-07 00:55:18 +0800

    make search results prettier

--- lib/builder/result_text_builder.dart
+++ lib/builder/result_text_builder.dart
@@ -5,18 +5,20 @@ class ResultTextBuilder {
   final int section;
   final String instructor;
   final double fontSize;
+  final Color textColor;
 
   ResultTextBuilder({
     this.title,
     this.section,
     this.instructor,
-    this.fontSize
+    this.fontSize,
+    this.textColor
   });
 
   Widget buildResultText() {
     return Text(
       '${title}_$section $instructor',
-      style: TextStyle(fontSize: fontSize)
+      style: TextStyle(fontSize: fontSize, color: textColor)
     );
   }
 }
